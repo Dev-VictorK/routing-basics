231eb2472383b90042756c284a1cef7bfc64c1ef
Victor Bundi <victorbundi.dev@gmail.com>
2025-06-13 15:54:37 +0300

    Stateless components

--- src/App.js
+++ src/App.js
@@ -19,10 +19,29 @@ class App extends React.Component {
           </li>
         </ul>
         <hr/>
-        
+
       </div>
     );
   }
 }
+
+const Atlantic = () => {
+  <div>
+    <h3>Atlantic Ocean</h3>
+    <p>The Atlantic Ocean covers approximately 1/5th of The
+      surface of the earth
+    </p>
+  </div>
+};
+
+const Pacific = () => {
+  <div>
+    <h3>Pacific Ocean</h3>
+    <p>
+      Ferdinande Magellan, a portuguese explorer, named the Ocean
+      `mar pacifico` in 1521, which means peaceful sea.
+    </p>
+  </div>
+};
 
 export default App;
